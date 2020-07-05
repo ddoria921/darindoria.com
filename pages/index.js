7fe9377f5c6dad0f03e13559cd7d9f30cdc8e97b
Darin Doria <ddoria921@gmail.com>
2020-07-05 17:53:17 -0400

    Add dark mode to home page

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,19 @@ export default function Home() {
           <ul>
             <li>
               <Link href="/work-journal">
-                <a className="text-lg text-blue-700 underline">Work Journal</a>
+                <a className="text-lg text-blue-700 underline dark:text-indigo-400">
+                  Work Journal
+                </a>
               </Link>
             </li>
           </ul>
         </nav>
       </header>
       <main className="pt-8">
-        <h1 className="text-3xl">🚧 Under Construction 🚧</h1>
-        <p className="text-lg mt-4">
+        <h1 className="text-3xl dark:text-gray-200">
+          🚧 Under Construction 🚧
+        </h1>
+        <p className="text-lg mt-4 dark:text-gray-200">
           Hi, I'm Darin. A frontend software engineer working out of Orlando,
           FL. This website is currently a work in progress, but in the meantime
           you can find me online at any of the links below.
@@ -24,21 +28,21 @@ export default function Home() {
       </main>
       <footer className="py-8">
         <a
-          className="text-lg text-blue-700 underline"
+          className="text-lg text-blue-700 underline dark:text-indigo-400"
           href="https://twitter.com/_darindoria"
         >
           twitter
         </a>
         <span className="mx-2">&middot;</span>
         <a
-          className="text-lg text-blue-700 underline"
+          className="text-lg text-blue-700 underline dark:text-indigo-400"
           href="https://github.com/ddoria921"
         >
           github
         </a>
         <span className="mx-2">&middot;</span>
         <a
-          className="text-lg text-blue-700 underline"
+          className="text-lg text-blue-700 underline dark:text-indigo-400"
           href="https://www.linkedin.com/in/darindoria"
         >
           linkedin
