37514df073050e661d7a4f3b3e7a8e696dadb39a
Darin Doria <ddoria921@gmail.com>
2020-07-28 01:32:13 -0400

    Remove dark mode styling from blog section

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
-import "../styles/index.css";
-
 import Head from "next/head";
+import "../styles/index.css";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  let pageClass = "";
+  let pageStyle = {};
+
+  if (router.pathname.includes("/work-journal")) {
+    pageClass = "light:bg-gray-100";
+  }
+
+  if (!router.pathname.includes("/blog")) {
+    pageStyle = { backgroundColor: "var(--dark-bg-color)" };
+  }
+
   return (
     <>
       <Head>
@@ -10,7 +22,9 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Personal website of Darin Doria." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <div className={`min-h-screen ${pageClass}`} style={pageStyle}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="light:bg-gray-100">
+        <body>
           <Main />
           <NextScript />
         </body>
