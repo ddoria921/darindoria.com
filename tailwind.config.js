ebb321993e1facc4141e3ec6fdcb82315fe80fce
Darin Doria <ddoria921@gmail.com>
2020-07-29 01:01:24 -0400

    Add code colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
         dark: { raw: "(prefers-color-scheme: dark)" },
         light: { raw: "(prefers-color-scheme: light)" },
       },
+      colors: {
+        code: {
+          green: "#b5f4a5",
+          yellow: "#ffe484",
+          purple: "#d9a9ff",
+          red: "#ff8383",
+          blue: "#93ddfd",
+          white: "#fff",
+        },
+      },
     },
   },
   variants: {
