277674ff9bf5074acdd6ae4639b0d2ac3be2295e
Darin Doria <ddoria921@gmail.com>
2020-07-29 00:55:31 -0400

    Add Image component

---
+++ components/Image.js
@@ -0,0 +1,9 @@
+export default function ({ alt, caption, src }) {
+  const altText = alt || caption;
+  return (
+    <figure>
+      <img src={src} alt={altText} />
+      {caption && <figcaption className="text-center">{caption}</figcaption>}
+    </figure>
+  );
+}
