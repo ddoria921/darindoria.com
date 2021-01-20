1a04f16462b9c4c1ef67f8ef9b808afb77b79cf1
Darin Doria <ddoria@wistia.com>
2021-01-19 22:29:24 -0500

    Push emoji as favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,12 +11,15 @@ function MyApp({ Component, pageProps }) {
     pageClass = "light:bg-gray-100";
   }
 
+  const svgIcon =
+    'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">⚡️</text></svg>';
+
   return (
     <>
       <Head>
         <title>Darin Doria</title>
         <meta name="description" content="Personal website of Darin Doria." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={svgIcon} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {process.env.NODE_ENV === "production" && (
           <script
