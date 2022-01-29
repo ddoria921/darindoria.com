4be967dbcb6f8f28790c3dc0f722165a089b15e8
Darin Doria <darin@Darins-MacBook-Pro.local>
2022-01-29 16:04:32 -0500

    Design tweaking

--- pages/work-journal.js
+++ pages/work-journal.js
@@ -12,16 +12,17 @@ export default function WorkJournal({ notionJournalEntries }) {
         <title>Work Journal - Darin Doria</title>
         <meta
           name="description"
-          content="A weekly record of notable things going on with work and side projects, plus general learnings."
+          content="A weekly record of notable things going on with work and side projects. Plus some general learnings."
         />
       </Head>
 
-      <div>
+      <div className="min-h-screen">
         <Header />
         <div className="pt-8">
           <h3 className="mt-2 mx-auto text-3xl leading-8 font-extrabold tracking-tight text-gray-900 w-full text-center md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg sm:text-4xl sm:leading-10 dark:text-gray-200">
             Work Journal
           </h3>
+
           <ul className="relative mx-auto mt-6 pl-12 w-full sm:pl-40 md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg xl:pl-56 xl:pr-24">
             {notionJournalEntries.map((entry, i) => (
               <VisibilitySensor
